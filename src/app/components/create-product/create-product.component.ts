d46a3e1a36cd55b3666f6e7483657c22e391500a
Ivgome <93914358+ivgome@users.noreply.github.com>
2022-06-30 10:56:59 +0200

    sizes

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-product',
   templateUrl: './create-product.component.html',
-  styleUrls: ['./create-product.component.css'],
 })
 export class CreateProductComponent {
   createProduct() {
